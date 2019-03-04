94bfb83f40cc184915757ab3ab3ace4b2cd71452
andrewprt <andrew.prawira.tunggono@gmail.com>
2019-03-04 21:49:34 +0700

    Updates

---
+++ precache-manifest.5ba96b5055c200858945c88b017e1af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9fa71fd449ffc1f9a70",
+    "url": "/facerecognitionbrain/static/js/runtime~main.a9fa71fd.js"
+  },
+  {
+    "revision": "9b25601938217e481aaa",
+    "url": "/facerecognitionbrain/static/js/main.9b256019.chunk.js"
+  },
+  {
+    "revision": "7a8073b14b0240c27829",
+    "url": "/facerecognitionbrain/static/js/2.7a8073b1.chunk.js"
+  },
+  {
+    "revision": "9b25601938217e481aaa",
+    "url": "/facerecognitionbrain/static/css/main.64eb9977.chunk.css"
+  },
+  {
+    "revision": "7a8073b14b0240c27829",
+    "url": "/facerecognitionbrain/static/css/2.86e2b361.chunk.css"
+  },
+  {
+    "revision": "caf8176ccd49c9adf3fc39fd3d0afef0",
+    "url": "/facerecognitionbrain/index.html"
+  }
+];
